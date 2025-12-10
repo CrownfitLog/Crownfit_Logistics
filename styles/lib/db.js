f2b1418e6870f6d02b94621f6c3d41bbc1de6b56
CrownfitLog <adeleke.adebowale@crownfitlogistics.com>
2025-12-10 13:26:57 -0600

    Create db.js

---
+++ styles/lib/db.js
@@ -0,0 +1,12 @@
+// lib/db.js
+import pkg from 'pg';
+const { Pool } = pkg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
+//Asmall Postgres helper using pg.
